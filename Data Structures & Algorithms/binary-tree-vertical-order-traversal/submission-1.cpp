97a51823c69f714ed56d61935e66537f07743e05
Babisca Paul <114242470+BabiscaPaul@users.noreply.github.com>
2026-05-09 17:18:40 +0300

    Add: binary-tree-vertical-order-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-vertical-order-traversal/submission-1.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        if (root == nullptr)    
+            return { };
+
+        vector<vector<int>> res{ };
+        map<int, vector<int>> levels{ };
+        queue<std::pair<TreeNode*, int>> q;
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            auto front = q.front();
+            if (front.first->left) q.push({front.first->left, front.second - 1});
+            if (front.first->right) q.push({front.first->right, front.second + 1});
+
+            q.pop();
+            
+            levels[front.second].push_back(front.first->val);
+        }
+
+        for (auto [key, val]: levels) {
+            res.push_back(val);
+        }
+
+        return res;
+    }
+};
