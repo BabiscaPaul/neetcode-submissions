c82128ef3ab3a268408a40880eeafa100310ae40
Babisca Paul <114242470+BabiscaPaul@users.noreply.github.com>
2026-05-11 11:52:34 +0300

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <vector> 
+using std::vector;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int l = 0, r = static_cast<int>(matrix.size()) - 1;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            const auto& currVec = matrix[mid];
+            
+            bool found = std::binary_search(currVec.begin(), currVec.end(), target);
+            if (found) return true;
+
+            if (target < currVec.front()) r = mid - 1;
+            else l = mid + 1;
+        }
+
+        return false;
+    }
+};
