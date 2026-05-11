28a18527468239c11040bbcdb0e121c8a5902eef
Babisca Paul <114242470+BabiscaPaul@users.noreply.github.com>
2026-05-11 12:51:26 +0300

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <numeric>
+#include <vector>
+using std::vector;
+
+class Solution {
+private:
+    bool check(vector<int>& weights, int days, int capacity) {
+        int group = 0, running_days = 1;
+
+        for (const int weight: weights) {
+            if (group + weight > capacity) {
+                group = 0;
+                running_days++;
+            }
+            group += weight;
+        }
+
+        return running_days <= days;
+    }
+
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l = *std::max_element(weights.begin(), weights.end());
+        int r = std::accumulate(weights.begin(), weights.end(), 0);
+        int res = r;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+
+            if (check(weights, days, mid)) {
+                res = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return res;
+    }
+};
