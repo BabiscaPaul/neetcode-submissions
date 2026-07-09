fa7616cf856a07e5b9b503e247c507aee3a67f70
Babisca Paul <114242470+BabiscaPaul@users.noreply.github.com>
2026-07-09 17:18:24 +0300

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.cpp
@@ -0,0 +1,18 @@
+#include <unordered_set>
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_set<int> seen{};
+
+        for (int num: nums) {
+            if (seen.contains(num))
+                return true;
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
